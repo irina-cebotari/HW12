17dc18d35f778f659d6cbb9017167028d28b74d6
irina-cebotari <irina.cebotari0301@gmail.com>
2024-03-04 20:42:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// const str = 'string'
+// let num = 6
+// let bool = true
+// let und = undefined
+// let nul = null
+// let bigInt = bigInt(999)
+// let symb = Symbol('')
+
+// const obj = {}
+
+// console.log(typeof str)
+
+let num1 = 27
+let num2 = 3
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+
+// num1++
+// console.log(num1++)
+
+// num1--
+// console.log(num1--)
+
+console.log(num1 ** num2)
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+
+console.log(!num1)
+
+console.log(num1 == num2)
+console.log(num1 === num2)
